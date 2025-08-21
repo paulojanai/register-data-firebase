1889097f1c2f159782c2e39ce7e458ccc499d913
pjanai <paulojanai.m.s@gmail.com>
2025-08-20 23:08:57 -0300

    feat(school): new value added of "Zona" in script and .csv

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // script.js
-import fs from 'fs';
-import csv from 'csv-parser';
-import dotenv from 'dotenv';
+import fs from "fs";
+import csv from "csv-parser";
+import dotenv from "dotenv";
 
-import { initializeApp } from 'firebase/app';
-import { getDatabase, onValue, ref, set } from 'firebase/database';
+import { initializeApp } from "firebase/app";
+import { getDatabase, onValue, ref, set } from "firebase/database";
 
 dotenv.config();
 
@@ -27,42 +27,43 @@ const db = getDatabase(app);
 function importarSchoolsCSV(filePath) {
   const schools = [];
 
-  fs.createReadStream(filePath, { encoding: 'utf8' })
+  fs.createReadStream(filePath, { encoding: "utf8" })
     .pipe(
       csv({
-        mapHeaders: ({ header }) => header.trim().replace(/^\ufeff/, ''),
+        mapHeaders: ({ header }) => header.trim().replace(/^\ufeff/, ""),
       })
     )
-    .on('data', (row) => {
+    .on("data", (row) => {
       // Cada "row" é um objeto com as colunas do CSV
       schools.push(row);
     })
-    .on('end', async () => {
+    .on("end", async () => {
       console.log(`📄 CSV carregado com ${schools.length} escolas.`);
       // return console.log(schools);
 
       for (const school of schools) {
         const schoolRef = ref(db, `schools/${school.inep}`);
         await set(schoolRef, {
-          district: school.district ?? '',
-          cnpj: school.cnpj ?? '',
-          name: school.name ?? '',
-          address: school.address ?? '',
+          district: school.district.toUpperCase() ?? "",
+          cnpj: school.cnpj.toUpperCase() ?? "",
+          name: school.name.toUpperCase() ?? "",
+          address: school.address.toUpperCase() ?? "",
+          area: school.area.toUpperCase() ?? "",
           working: school.working
-            ? school.working.toUpperCase() === 'FUNCIONANDO'
+            ? school.working.toUpperCase() === "FUNCIONANDO"
             : false,
         });
         console.log(`✅ Escola ${school.name} adicionada com sucesso!`);
       }
 
-      console.log('🎉 Importação finalizada!');
+      console.log("🎉 Importação finalizada!");
     });
 }
 
 const SearchSchoolParalisada = async () => {
   try {
     onValue(
-      ref(db, 'schools/'),
+      ref(db, "schools/"),
       (data) => {
         if (!data.exists()) return;
 
@@ -99,6 +100,6 @@ const SearchSchoolParalisada = async () => {
 // };
 
 // Executa a função
-importarSchoolsCSV('schools.csv');
+importarSchoolsCSV("schools.csv");
 // SearchSchoolParalisada();
 // AllSkills();
